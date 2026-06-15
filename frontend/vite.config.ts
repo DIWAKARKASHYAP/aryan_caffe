a63f6aa16b7892e919ee0091f27dc9fc23be3a24
DIWAKAR <didk9675@gmail.com>
2026-06-15 15:42:04 +0530

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://localhost:3001',
+        target: 'https://aryan-caffe-api.vercel.app',
         changeOrigin: true,
       },
     },
